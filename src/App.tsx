e8c94962a9f8f1bf55688cd2954f62f589a0f0d0
ronnie-allen <ronnieallen2005@gmail.com>
2025-03-18 10:46:42 +0530

    Layout fixed

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,24 @@ const App: React.FC = () => {
     <div className="h-screen w-screen bg-gray-900 text-white flex flex-col items-center justify-center p-6">
       <h1 className="text-3xl font-bold mb-6 text-blue-400">Blind Navigation System</h1>
 
-      <div className="grid grid-cols-2 gap-6 w-[90vw] h-[85vh]">
-        {/* Left Side (Webcam + Logs) */}
-        <div className="flex flex-col gap-6 w-full h-full">
-          <div className="box-component h-3/5">
+      {/* Adjusted Grid Layout */}
+      <div className="grid grid-cols-3 gap-6 w-[90vw] h-[85vh]">
+        {/* Left Side (Wider Webcam + Logs) */}
+        <div className="flex flex-col gap-6 col-span-2 w-full h-full">
+          <div className="box-component h-4/5">
             <WebcamFeed />
           </div>
-          <div className="box-component h-2/5">
+          <div className="box-component h-1/5">
             <Logs detectedObjects={[]} />
           </div>
         </div>
 
-        {/* Right Side (Static Text + Image) */}
-        <div className="flex flex-col gap-6 w-full h-full">
-          <div className="box-component h-4/5">
+        {/* Right Side (Narrower Static Text + Image) */}
+        <div className="flex flex-col gap-6 col-span-1 w-full h-full">
+          <div className="box-component h-3/5">
             <StaticText />
           </div>
-          <div className="box-component h-1/5">
+          <div className="box-component h-2/5">
             <StaticImage src="/images/sample.png" alt="System Feature" />
           </div>
         </div>
